5ace9a804dbb4e9699dd3ff9f7cf58b8a826eda7
kiza <zstojkovic10-@gmail.com>
2023-09-04 18:18:28 +0200

    Added header, navbar, and footer component with styles

--- js/main.js
+++ js/main.js
@@ -1,5 +1,15 @@
 // main.js
+var validRegex = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
 
-$(document).ready(function (){
-	console.log('document is ready bitch');
+
+$('.button').on('click', function(e) {
+	e.preventDefault();
+
+	var email = $('#email');
+	if (email[0].value.match(validRegex)) {
+		alert("Thank you for subscribing!");
+		} else {
+		alert("Please enter a valid email.")
+		};
 });
+
